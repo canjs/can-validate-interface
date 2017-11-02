07afd28a78a0a3dae39159b4ad5876af592cb3c7
imasustink <austin@austinkurpuis.com>
2017-11-02 13:39:56 -0700

    Use QUnit and testee to run tests
    This module is being added to canjs so the tests must be run with QUnit

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+'use strict';
+
+function flatten(arrays) {
+	return arrays.reduce(function(ret, val) {
+		return ret.concat(val);
+	}, []);
+}
+
 // return a function that validates it's argument has all the properties in the interfacePropArrays
 function makeInterfaceValidator(interfacePropArrays) {
 	var props = flatten(interfacePropArrays);
 
 	return function(base) {
 			var missingProps = props.reduce(function(missing, prop) {
-				return prop in base ? missing : missing.concat(prop)
+				return prop in base ? missing : missing.concat(prop);
 			}, []);
 
 		return missingProps.length ? {message:"missing expected properties", related: missingProps} : undefined;
-	}
-}
-
-function flatten(arrays) {
-	return arrays.reduce(function(ret, val) {
-		return ret.concat(val)
-	}, [])
+	};
 }
 
 module.exports = makeInterfaceValidator;
