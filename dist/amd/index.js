6a55e2e462547566923823bb9f7cd15bbeede7e5
Kevin Phillips <kevin@bitovi.com>
2018-01-29 14:19:37 -0600

    1.0.0

---
+++ dist/amd/index.js
@@ -0,0 +1,22 @@
+/*can-validate-interface@1.0.0-pre.2#index*/
+define(function (require, exports, module) {
+    'use strict';
+    function flatten(arrays) {
+        return arrays.reduce(function (ret, val) {
+            return ret.concat(val);
+        }, []);
+    }
+    function makeInterfaceValidator(interfacePropArrays) {
+        var props = flatten(interfacePropArrays);
+        return function (base) {
+            var missingProps = props.reduce(function (missing, prop) {
+                return prop in base ? missing : missing.concat(prop);
+            }, []);
+            return missingProps.length ? {
+                message: 'missing expected properties',
+                related: missingProps
+            } : undefined;
+        };
+    }
+    module.exports = makeInterfaceValidator;
+});
---
+++ dist/cjs/index.js
@@ -0,0 +1,20 @@
+/*can-validate-interface@1.0.0-pre.2#index*/
+'use strict';
+function flatten(arrays) {
+    return arrays.reduce(function (ret, val) {
+        return ret.concat(val);
+    }, []);
+}
+function makeInterfaceValidator(interfacePropArrays) {
+    var props = flatten(interfacePropArrays);
+    return function (base) {
+        var missingProps = props.reduce(function (missing, prop) {
+            return prop in base ? missing : missing.concat(prop);
+        }, []);
+        return missingProps.length ? {
+            message: 'missing expected properties',
+            related: missingProps
+        } : undefined;
+    };
+}
+module.exports = makeInterfaceValidator;
